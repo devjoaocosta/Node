28ec7a9d35aebbf5bac86e4173515e2edada3da4
João Costa <joaovitorcostaoliveira@gmail.com>
2024-02-24 22:43:17 -0300

    create read

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
-import { createServer } from 'node:http'
+import { DatabaseMemory } from './database-memory.js'
+import { fastify } from 'fastify'
 
-const server = createServer((request, response) => {
-    response.write('ola')
-    return response.end()
-} )
+const server = fastify()
 
-server.listen(3333)
+const database = new DatabaseMemory
+// criar videos
+server.post('/videos', (request, reply) => {
+    const { title, description, duration} = request.body
+    
+    database.create({
+        title: title,
+        description: description,
+        duration: duration
+    })
+    console.log(database.list())
 
+    return reply.status(201).send()
+})
+
+server.get('/videos', (request, reply) => {
+    const videos = database.list()
+
+    return reply.send(videos)
+})
+// atualizar video
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.videoId
+    const { title, description, duration} = request.body
+
+    const video = database.update(videoId, {
+        title: title,
+        description: description,
+        duration: duration
+    })
+})
+// deletar video
+server.delete('/videos/:id', () => {
+    
+})
+server.listen({
+    port: 3333,
+})
